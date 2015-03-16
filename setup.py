a04a034850e3c567295a920af5031104786c8c8a
simonefenix <simone.murzilli@fao.org>
2015-03-16 15:27:29 +0100

    adde watchdog version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='GeobricksGeocoding',
-    version='0.1.2',
+    version='0.1.3',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
@@ -11,6 +11,7 @@ setup(
     long_description=open('README.md').read(),
     description='Geobricks library to manage geocoding.',
     install_requires=[
+        'watchdog',
         "flask",
         'flask-cors',
         'geopy'
