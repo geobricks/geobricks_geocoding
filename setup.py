a04be0de0dd3fdcf25425cb9614aba44addf8722
simonefenix <simone.murzilli@fao.org>
2015-07-15 13:43:43 +0200

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='GeobricksGeocoding',
-    version='0.1.4',
+    version='0.1.5',
     author='Simone Murzilli; Guido Barbaglia',
     author_email='geobrickspy@gmail.com',
     packages=find_packages(),
